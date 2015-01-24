edb81a7d7c5533bfec44d771b9e5f0b3b03cd0c1
roffl <misorinak@gmail.com>
2015-01-24 17:01:22 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css')
+    connect = require('gulp-connect'),
+    wiredep = require('wiredep').stream,
+    clean = require('gulp-clean'),
     opn = require('opn');
 
+
+//localhost
 gulp.task('connect', function() {
     connect.server({
         root: 'app',
@@ -10,6 +18,16 @@ gulp.task('connect', function() {
     opn('http://localhost:8080/');
 });
 
+//Bower
+gulp.task('wiredep', function () { 
+    gulp.src('app/*.html')
+    .pipe(wiredep({
+        directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+//html css
 gulp.task('htmlCss', function () {
     gulp.src('./app/*.html')
         .pipe(connect.reload());
@@ -25,4 +43,25 @@ gulp.task('watch', function () {
     gulp.watch(['./app/css/*.css'], ['htmlCss']);
 });
 
+
+
+//DIST
+gulp.task('dist', function () {
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+// Очистка
+gulp.task('clean', function () {
+return gulp.src('dist', {read: false}).pipe(clean());
+});
+
+//Default
 gulp.task('default', ['connect', 'watch']);
